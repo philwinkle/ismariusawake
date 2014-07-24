3360f30b030625caa8764b39e76717a1848c04ee
Phillip Jackson <philwinkle@gmail.com>
2014-07-24 01:30:16 -0400

    Added new styles and tweaked templates

--- index.php
+++ index.php
@@ -8,20 +8,22 @@ $feed = Zend\Feed\Reader\Reader::import('http://magento.stackexchange.com/feeds/
 $now = new DateTime();
 
 foreach($feed as $entry){
-  $lastSeen = $now->diff($entry->getDateModified());
-  break;
+    $lastSeen = $now->diff($entry->getDateModified());
+    break;
 }
 
 $output = sprintf('%s days, %s hours, %s minutes and %s seconds',
-$lastSeen->d,
-$lastSeen->h,
-$lastSeen->i,
-$lastSeen->s
+    $lastSeen->d,
+    $lastSeen->h,
+    $lastSeen->i,
+    $lastSeen->s
 );
 
-$isSleeping = $lastSeen->h > 3 ? 'is sleeping' : 'is awake';
+$isSleeping = $lastSeen->h > 3;
+$asleep =  $isSleeping ? 'is sleeping' : 'is awake';
+$bodyclass = $isSleeping ? 'night' : 'day';
 
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
 
-echo $twig->render('index.html',array('time'=>$output,'status'=>$isSleeping));
+echo $twig->render('index.html',array('time'=>$output,'status'=>$asleep, 'bodyclass'=>$bodyclass));
