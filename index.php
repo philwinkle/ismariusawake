e3feddbdc4922df488d1ba9e3577854f5f20bc8e
Phillip Jackson <philwinkle@gmail.com>
2014-07-24 03:39:50 +0000

    Fonts

--- index.php
+++ index.php
@@ -21,9 +21,17 @@ $lastSeen->s
 
 $isSleeping = $lastSeen->h > 3 ? 'is sleeping' : 'is awake';
 ?>
+<style>
+h1, h2 {
+font-family: "futura-pt";
+}
+</style>
 <h1>
 Marius was last seen <?php echo $output; ?> ago
 </h1>
 <h2>
 Therefore it is likely that Marius <?php echo $isSleeping; ?>
 </h2>
+
+<script type="text/javascript" src="//use.typekit.net/zgx5pld.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
